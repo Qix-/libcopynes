bfeeb907ea85e79befc9a9e2a825332fdb291d8a
Dave Huseby <dave@linuxprogrammer.org>
2009-03-07 23:39:17 +0000

    some details

--- copynes.h
+++ copynes.h
@@ -1,9 +1,9 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
- * main.c
+ * libcopynes.h
  * Copyright (C) David Huseby 2009 <dave@linuxprogrammer.org>
  * 
- * libcopynes.h is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
