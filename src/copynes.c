8939cc5e0ee88587da7b4e4fd16872a641f30208
Dave Huseby <dave@linuxprogrammer.org>
2012-05-31 09:02:29 -0700

    switching over to termios.h for better posix compatibility

--- src/copynes.c
+++ src/copynes.c
@@ -24,12 +24,14 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <termio.h>
+#include <termios.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/select.h> 
+#include <sys/ioctl.h>
+
 #include "copynes.h"
 
 #define _POSIX_SOURCE 1
