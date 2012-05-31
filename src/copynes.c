5a2e56df786cd8594af24efc069ced9974e0eba7
Dave Huseby <dave@linuxprogrammer.org>
2012-05-31 09:17:28 -0700

    adding an include for system-specific termios.h too

--- src/copynes.c
+++ src/copynes.c
@@ -24,13 +24,14 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <termios.h>
+#include <termios.h>		/* POSIX compiant terminal I/O bits */
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/select.h> 
 #include <sys/ioctl.h>
+#include <sys/termios.h>	/* platform specific terminal I/O bits */
 
 #include "copynes.h"
 
